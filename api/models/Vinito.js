397fff9ecc36816c259acb2742eb72b36d7daf9e
garcianavalon <garcianavalon@gmail.com>
2016-03-21 13:00:07 +0100

    added attributes to vinito

--- api/models/Vinito.js
+++ api/models/Vinito.js
@@ -1,14 +1,37 @@
 /**
  * Vinito.js
  *
- * @description :: TODO: You might write a short summary of how this model works and what it represents here.
+ * @description :: One vinito rico
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
 module.exports = {
 
   attributes: {
-
+  	name: {
+  		type: 'string',
+  		size: 30,
+      required: true
+  	},
+  	description: {
+  		type: 'string',
+  		size: 300,
+      required: true
+  	},
+  	price: {
+  		type: 'float',
+      required: true
+  	},
+  	vintage: {
+	  	type: 'integer', // añada, no es coña xD
+	  	required: true
+	  },
+		type: {
+      type: 'string',
+      enum: ['tinto', 'blanco', 'sobremadre', 'rosado', 'dulce', 'espumoso'],
+      required: true,
+      size: 16
+    }
   }
 };
 
